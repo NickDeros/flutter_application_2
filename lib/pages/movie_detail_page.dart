60b134128b9b7002fb6eef976b5a1ad702a06dc9
NickDeros <nicolasderosa002@gmail.com>
2024-02-20 10:53:17 +0100

    Youtube Player Added

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -25,65 +25,80 @@ class _MovieDetailPageState extends State<MovieDetailPage> {
         future: movieController.getMovie(widget.movieId),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            final item = snapshot.data['production_companies'];
-            print(item.toString());
-            print("hasdata");
+            final production_companies = snapshot.data['production_companies'];
+            for (var company in production_companies) {
+              print(company['name']);
+            }
+            print(production_companies.toString());
+            print("movie detail data");
             print(snapshot.data['production_companies'].length);
-            return Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Text(
-                    snapshot.data["title"].toString(),
-                    style: GoogleFonts.aBeeZee(
-                      color: Colors.white,
-                      fontSize: 25,
+
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Text(
+                      snapshot.data["title"].toString(),
+                      style: GoogleFonts.aBeeZee(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      url + snapshot.data!['backdrop_path'],
-                      filterQuality: FilterQuality.low,
-                      height: 300,
-                      width: 380,
-                      fit: BoxFit.cover,
+                    const SizedBox(height: 20),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: Image.network(
+                        url + snapshot.data!['backdrop_path'],
+                        filterQuality: FilterQuality.low,
+                        height: 300,
+                        width: 380,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        snapshot.data["vote_average"].toString(),
-                        style: GoogleFonts.aBeeZee(
-                          color: Colors.white,
-                          fontSize: 20,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          snapshot.data["vote_average"].toString(),
+                          style: GoogleFonts.aBeeZee(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
                         ),
-                      ),
-                      const Icon(
-                        Icons.star,
-                        color: Colors.yellow,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    snapshot.data['overview'].toString(),
-                    style: GoogleFonts.aBeeZee(fontSize: 16.5),
-                  ),
-                  const SizedBox(height: 20),
+                        const Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      snapshot.data['overview'].toString(),
+                      style: GoogleFonts.aBeeZee(fontSize: 16.5),
+                    ),
+                    const SizedBox(height: 20),
+                
+                    // ListView(
+                    //   physics: NeverScrollableScrollPhysics(),
+                    //   shrinkWrap: true,
+                    //   children: <Widget>[
+                    //     Text('ciao'),
+                    //     Text('gigio'),
+                    //   ],    
+                    // )
               
-                  ListView(
-                    physics: NeverScrollableScrollPhysics(),
+                    ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
                     shrinkWrap: true,
-                    children: <Widget>[
-                      Text('ciao'),
-                      Text('gigio'),
-                    ],
-                  
+                    itemCount: production_companies.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(title: Text(production_companies[index]['name'], style: Theme.of(context).textTheme.titleMedium));
+                    },
                   )
-                ],
+              
+                  ],
+                ),
               ),
             );
           } else if (snapshot.hasError) {
